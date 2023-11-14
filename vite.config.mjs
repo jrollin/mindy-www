7657857ce886678f05435aa6648dc8ad8b2f9d04
jrollin <1086565+jrollin@users.noreply.github.com>
2023-11-14 18:20:41 +0100

    build: add env in vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import path from 'path';
 // https://vitejs.dev/config/
-export default defineConfig({
-    resolve: {
-        alias: {
-            '@app': path.resolve(__dirname, './src'),
-            '@usecases': path.resolve(__dirname, './core/usecases/'),
-            '@domains': path.resolve(__dirname, './core/domains/'),
-            '@components': path.resolve(__dirname, './src/components'),
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, './');
+    return {
+        resolve: {
+            alias: {
+                '@app': path.resolve(__dirname, './src'),
+                '@usecases': path.resolve(__dirname, './core/usecases/'),
+                '@domains': path.resolve(__dirname, './core/domains/'),
+                '@components': path.resolve(__dirname, './src/components'),
+            },
         },
-    },
-    plugins: [react(), nodePolyfills()],
+        plugins: [react(), nodePolyfills()],
+        define: {
+            VITE_API_URL: env.VITE_API_URL,
+        },
+    };
 });
